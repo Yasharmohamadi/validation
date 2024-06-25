8bb948c7e1fa3b001bd7f9a7e182ead3f5a994f1
Yashar <yashar22mo@gmail.com>
2024-06-25 14:28:24 +0330

    Coding script

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,32 +11,78 @@ export default class Forms extends React.Component {
 			allValid: false,
 			sumbitted: false,
 		};
+
+		this.submitHandler = this.submitHandler.bind(this);
+		this.firstNameHandler = this.firstNameHandler.bind(this);
+		this.lastNameHandler = this.lastNameHandler.bind(this);
+	}
+
+	submitHandler(event) {
+		event.preventDefault();
+		this.setState({
+			sumbitted: true,
+		});
+
+		if (this.state.firstName && this.state.lastName) {
+			this.setState({
+				allValid: true,
+			});
+
+			setTimeout(() => {
+				this.setState({
+					allValid: false,
+				});
+			}, 3000);
+		}
+	}
+
+	firstNameHandler(event) {
+		this.setState({
+			firstName: event.target.value,
+		});
+	}
+	lastNameHandler(event) {
+		this.setState({
+			lastName: event.target.value,
+		});
 	}
 
 	render() {
 		return (
 			<div className="form-container">
-				<form className="register-form">
+				<form className="register-form" onSubmit={this.submitHandler}>
 					{/* Uncomment the next line to show the success message */}
-					{/* <div className="success-message">Success! Thank you for registering</div> */}
+					{this.state.sumbitted && this.state.allValid && (
+						<div className="success-message">
+							Success! Thank you for registering
+						</div>
+					)}
 					<input
 						id="first-name"
+						value={this.state.firstName}
 						className="form-field"
 						type="text"
 						placeholder="First Name"
 						name="firstName"
+						onChange={this.firstNameHandler}
 					/>
 					{/* Uncomment the next line to show the error message */}
-					{/* <span id="first-name-error">Please enter a first name</span> */}
+					{this.state.sumbitted && !this.state.firstName && (
+						<span id="first-name-error">Please enter a first name</span>
+					)}
 					<input
 						id="last-name"
+						value={this.state.lastName}
 						className="form-field"
 						type="text"
 						placeholder="Last Name"
 						name="lastName"
+						onChange={this.lastNameHandler}
 					/>
 					{/* Uncomment the next line to show the error message */}
-					{/* <span id="last-name-error">Please enter a last name</span> */}
+					{this.state.sumbitted && this.state.lastName.length === 0 && (
+						<span id="last-name-error">Please enter a last name</span>
+					)}
 					<button className="form-field" type="submit">
 						Register
 					</button>
